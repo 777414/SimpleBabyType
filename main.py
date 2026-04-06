7ee6afa71011c1a1037b4666c8f526ca199d4a31
777414 <vladpupkin3@gmail.com>
2026-04-06 14:49:49 +0300

    Добавлен вывод одного квадратика и буквы

--- main.py
+++ main.py
@@ -10,17 +10,29 @@ HEIGHT = 400
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 100, 255)
+RED = (255, 0, 0)
+
 font = pygame.font.SysFont("Times New Roman", 60)
 
+clock = pygame.time.Clock()
+FPS = 60
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    txt = font.render("Hello!", True, (255,255,255))
-    screen.blit(txt, (100,100))
-
+    screen.fill(WHITE)
+    text = font.render("A", True, WHITE)
+    pygame.draw.rect(screen, BLUE, (180,180, 50,60))
+    text_rect = text.get_rect(center=(180 + 25, 180 + 30))
+    screen.blit(text, text_rect)
+    
+    pygame.display.flip()
     pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
